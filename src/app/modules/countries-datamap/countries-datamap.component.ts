4fa29061d564a768437381e06b2203767b7177b6
Jago <jago84@gmail.com>
2018-05-30 17:20:27 +0200

    Extracted tooltip generation function + enhancements + lint

--- src/app/modules/countries-datamap/countries-datamap.component.ts
+++ src/app/modules/countries-datamap/countries-datamap.component.ts
@@ -1,23 +1,22 @@
-import { Component, Input, OnInit, ViewChild, ElementRef } from "@angular/core";
-import "datamaps";
+import { Component, Input, OnInit, ViewChild, ElementRef } from '@angular/core';
+import 'datamaps';
 
 const DEFAULT_WIDTH = 500;
 const SIZE_FACTOR = 0.42;
-const COUNTRY = { MINE: "mycountry", SELECTED: "selected" };
+const COUNTRY = { MINE: 'mycountry', SELECTED: 'selected' };
 
 @Component({
-  selector: "countries-datamap",
-  templateUrl: "./countries-datamap.component.html",
-  styleUrls: ["./countries-datamap.component.css"]
+  selector: 'countries-datamap',
+  templateUrl: './countries-datamap.component.html',
+  styleUrls: ['./countries-datamap.component.css']
 })
 export class CountriesDatamapComponent implements OnInit {
-  @Input() mapData: any;
-  @Input() mapDefaultFill = "#afafaf";
-  @Input() countryColor = "#428bca";
-  @Input() selfCountryColor = "#5cb85c";
-  @Input() parentWidth = null;
+  @Input() mapData: any[] = [];
+  @Input() mapDefaultFill = '#afafaf';
+  @Input() countryColor = '#428bca';
+  @Input() selfCountryColor = '#5cb85c';
   @Input() multiUser = true;
-  availableWidth: number;
+
   processedData: object;
   renderedMap: DataMap;
 
@@ -43,14 +42,14 @@ export class CountriesDatamapComponent implements OnInit {
         let isHomeCountry = true;
         const usersInThisFlag = [];
 
-        //sort this flag by times. if self country, counts much less
+        // sort this flag by times. if self country, counts much less
         flagoccurrence.users.sort(flagUser => {
           return flagUser.isSelfCountry
             ? flagUser.times
             : flagUser.times * 1000;
         });
 
-        //collect users data and see if it is self country for all (cause it'll get different color)
+        // collect users data and see if it is self country for all (cause it'll get different color)
         flagoccurrence.users.forEach(flagUser => {
           isHomeCountry = isHomeCountry && flagUser.isSelfCountry;
           usersInThisFlag.push({
@@ -68,55 +67,22 @@ export class CountriesDatamapComponent implements OnInit {
       }
     });
 
-    if (this.parentWidth) {
-      if (this.mapContainer.nativeElement.parentElement) {
-        // parentElement
-        this.availableWidth = this.mapContainer.nativeElement.parentElement.offsetWidth;
-      } else {
-        this.availableWidth =
-          this.mapContainer.nativeElement.parentNode.offsetWidth > 0
-            ? this.mapContainer.nativeElement.parentNode.parentNode.offsetWidth
-            : DEFAULT_WIDTH;
-      }
-    } else {
-      this.availableWidth =
-        this.mapContainer.nativeElement.parentNode.offsetWidth > 0
-          ? this.mapContainer.nativeElement.parentNode.parentNode.offsetWidth
-          : DEFAULT_WIDTH;
-    }
-    const height = this.availableWidth * SIZE_FACTOR;
+    const availableWidth =
+      this.mapContainer.nativeElement.parentNode.offsetWidth > 0
+        ? this.mapContainer.nativeElement.parentNode.parentNode.offsetWidth
+        : DEFAULT_WIDTH;
+    const height = availableWidth * SIZE_FACTOR;
 
     // === START: DATAMAPS MAIN PART ===
     this.renderedMap = new DataMap({
       element: this.mapContainer.nativeElement,
-      width: this.availableWidth,
+      width: availableWidth,
       height,
       // projection: 'mercator',
       geographyConfig: {
-        //geographyConfig object can be removed to restore hover effects
+        // geographyConfig object can be removed to restore hover effects
         highlightOnHover: false,
-        popupTemplate: (geography: DataMapGeographyData, data) => {
-          let content = `<strong>${geography.properties.name}</strong>`;
-          if (data) {
-            if (!this.multiUser) {
-              if (data.users[0].showTimes) {
-                content = `${content}<br/>Times: ${data.users[0].times}`;
-              }
-            } else {
-              data.users.forEach(flagUser => {
-                if (flagUser.showTimes) {
-                  content = `${content}<br/>
-                    ${flagUser.name} ${flagUser.surname}:
-                    <strong>x${flagUser.times}</strong>`;
-                } else {
-                  content = `${content}<br/>
-                    ${flagUser.name} ${flagUser.surname} (self country)`;
-                }
-              });
-            }
-          }
-          return `<div class="hoverinfo">${content}</div>`;
-        }
+        popupTemplate: this.generateTooltip
       },
       fills: {
         defaultFill: this.mapDefaultFill,
@@ -126,6 +92,29 @@ export class CountriesDatamapComponent implements OnInit {
       data: this.processedData
     });
     // === END: DATAMAPS MAIN PART ===
+  }
+
+  generateTooltip(geography, data) {
+    let content = `<strong>${geography.properties.name}</strong>`;
+    if (data) {
+      if (!this.multiUser) {
+        if (data.users[0].showTimes) {
+          content = `${content}<br/>Times: ${data.users[0].times}`;
+        }
+      } else {
+        data.users.forEach(flagUser => {
+          if (flagUser.showTimes) {
+            content = `${content}<br/>
+              ${flagUser.name} ${flagUser.surname}:
+              <strong>x${flagUser.times}</strong>`;
+          } else {
+            content = `${content}<br/>
+              ${flagUser.name} ${flagUser.surname} (self country)`;
+          }
+        });
+      }
+    }
+    return `<div class="hoverinfo">${content}</div>`;
   }
 
   ngOnInit() {
